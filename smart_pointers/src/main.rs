45975017a7f29138477ac6f64522f858e2c4d0d6
Alex Astafyev <alex.astafyev@dualbootpartners.com>
2023-06-30 18:46:56 +0400

    implemented custom smart pointer, played with Deref trait

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -5,6 +5,23 @@ enum List {
 }
 
 use List::{Cons, Nil};
+use core::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
 
 fn main() {
     let b = Box::new(5);
@@ -12,4 +29,17 @@ fn main() {
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Nil))));
     println!("list {:?}", list);
+
+    let x = 5;
+    let y = MyBox::new(x);
+    println!("y = {}", *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    // deref coersion is working here
+    // &MyBox<String> -> &String -> &str
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
